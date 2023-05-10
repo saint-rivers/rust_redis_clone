83fd6cdd8ea8687f9a7ba764f9519711c066b5e9
dayan <eam.dayan@gmail.com>
2023-05-10 16:42:06 +0700

    working CLI

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,36 +1,102 @@
 use bytes::BytesMut;
+use clap::{Parser, Subcommand};
+
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpStream,
 };
 
+#[derive(Parser, Debug)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+}
+#[derive(Subcommand, Debug)]
+enum Command {
+    Get { key: String },
+    Set { key: String, value: String },
+}
+
 #[tokio::main]
 pub async fn main() -> Result<(), std::io::Error> {
-    let mut stream = TcpStream::connect("127.0.0.1:8081").await?;
-    // stream.write_all(b"set foo bar").await?;
-    stream.write_all(b"get foo").await?;
+    let args = Cli::parse();
+    let mut stream = TcpStream::connect("127.0.0.1:8081").await.unwrap();
 
-    let mut buf = BytesMut::with_capacity(1024);
-    let _length = stream.read_buf(&mut buf).await?;
-    match std::str::from_utf8(&mut buf) {
-        Ok(resp) => {
-            // if resp == "r Ok" {
-            //     println!("key updated");
-            // } else if resp == "Ok" {
-            //     println!("key set");
-            // }
+    match args.command {
+        Command::Set { key, value } => {
+            stream.write_all(b"set").await?;
+            stream.write_all(b" ").await?;
 
-            if resp == "" {
-                println!("no such key found");
-            } else {
-                println!("value: {}", resp);
+            stream.write_all(&key.as_bytes()).await?;
+            stream.write_all(b" ").await?;
+
+            stream.write_all(&value.as_bytes()).await?;
+            let mut buf = BytesMut::with_capacity(1024);
+            let _length = stream.read_buf(&mut buf).await?;
+            match std::str::from_utf8(&mut buf) {
+                Ok(resp) => {
+                    if resp == "r Ok" {
+                        println!("updated key");
+                    } else if resp == "Ok" {
+                        println!("key set");
+                    }
+                }
+                Err(err) => {
+                    // failed to convert bytes into string slice
+                    println!("error: {}", err);
+                }
             }
         }
-        Err(err) => {
-            // failed to convert bytes into string slice
-            println!("error: {}", err);
-        }
-    }
+        Command::Get { key } => {
+            stream.write_all(b"get").await?;
+            stream.write_all(b" ").await?;
 
-    Ok(())
+            stream.write_all(&key.as_bytes()).await?;
+
+            let mut buf = BytesMut::with_capacity(1024);
+            let _length = stream.read_buf(&mut buf).await?;
+            match std::str::from_utf8(&mut buf) {
+                Ok(resp) => {
+                    if resp == "" {
+                        println!("no such key found");
+                    } else {
+                        println!("key: {} => value: {}", key, resp);
+                    }
+                }
+                Err(_err) => {
+                    println!("in errr");
+                }
+            }
+        }
+    };
+    return Ok(());
 }
+
+// let mut stream = TcpStream::connect("127.0.0.1:8081").await?;
+// // stream.write_all(b"set foo bar").await?;
+// stream.write_all(b"get foo").await?;
+
+// let mut buf = BytesMut::with_capacity(1024);
+// let _length = stream.read_buf(&mut buf).await?;
+// match std::str::from_utf8(&mut buf) {
+//     Ok(resp) => {
+//         // if resp == "r Ok" {
+//         //     println!("key updated");
+//         // } else if resp == "Ok" {
+//         //     println!("key set");
+//         // }
+
+//         if resp == "" {
+//             println!("no such key found");
+//         } else {
+//             println!("value: {}", resp);
+//         }
+//     }
+//     Err(err) => {
+//         // failed to convert bytes into string slice
+//         println!("error: {}", err);
+//     }
+// }
+
+// Ok(())
+// }
